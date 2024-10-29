8d60dd0c93b9a090584631909db035d55a1d3093
marvie shoyoye <marvellousshoyoye@gmail.com>
2024-10-29 09:25:52 +0100

    i commented the reset password

--- src/routes/authroute.js
+++ src/routes/authroute.js
@@ -3,8 +3,8 @@ import verifyToken from "../auth/auth.js";
 import {
   UserSignup,
   UserLogin,
-  requestPasswordReset,
-  resetPassword,
+  // requestPasswordReset,
+  // resetPassword,
   getUserProfile,
   changePassword,
   updateUserProfile,
@@ -15,8 +15,8 @@ const router = express.Router();
 
 router.post("/signup", UserSignup);
 router.post("/login", UserLogin);
-router.post("/request-password-reset", requestPasswordReset);
-router.post("/reset-password", resetPassword);
+// router.post("/request-password-reset", requestPasswordReset);
+// router.post("/reset-password", resetPassword);
 router.get("/me", verifyToken, getUserProfile);
 router.post("/change-password", verifyToken, changePassword);
 router.patch("/update/:id", updateUserProfile);
